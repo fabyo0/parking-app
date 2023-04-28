1f3438e202395c37adf1e6f888333dd16e776461
Emre Dikmen <emredikmen002@gmail.com>
2023-04-29 02:23:21 +0300

    user login

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\V1\Auth\LoginController;
 use App\Http\Controllers\Api\V1\Auth\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -16,6 +17,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::post('auth/register', RegisterController::class);
+Route::post('auth/login', LoginController::class);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
