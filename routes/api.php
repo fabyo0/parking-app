2268639dd0b3c830be7710a141e684d3ff3e0d91
Emre Dikmen <emredikmen002@gmail.com>
2023-04-29 03:34:05 +0300

    install auth sanctum , get profile ,profile update & update password

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\Api\V1\Auth\LoginController;
+use App\Http\Controllers\Api\V1\Auth\LogoutController;
+use App\Http\Controllers\Api\V1\Auth\PasswordUpdateController;
+use App\Http\Controllers\Api\V1\Auth\ProfileController;
 use App\Http\Controllers\Api\V1\Auth\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -19,6 +22,10 @@ use Illuminate\Support\Facades\Route;
 Route::post('auth/register', RegisterController::class);
 Route::post('auth/login', LoginController::class);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('profile', [ProfileController::class, 'show']);
+    Route::put('profile', [ProfileController::class, 'update']);
+    Route::put('password', PasswordUpdateController::class);
+
+    Route::post('auth/logout', LogoutController::class);
 });
